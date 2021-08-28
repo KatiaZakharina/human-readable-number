37bf2d20ddcc3109a169fbdab99993231c95c694
Katia <katia.zakharina@gmail.com>
2021-08-28 22:05:27 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(num) {
+  const spelling = [
+      'zero',
+      'one',
+      'two',
+      'three',
+      'four',
+      'five',
+      'six',
+      'seven',
+      'eight',
+      'nine',
+      'ten',
+      'eleven',
+      'twelve',
+      'thirteen',
+      'fourteen',
+      'fifteen',
+      'sixteen',
+      'seventeen',
+      'eighteen',
+      'nineteen',
+    ],
+    prefix = ['', '', 'twen', 'thir', 'for', 'fif', 'six', 'seven', 'eigh', 'nine'];
+
+  if (num < 20) {
+    return spelling[num];
+  } else if (num < 100) {
+    return prefix[Math.floor(num / 10)] + 'ty' + (num % 10 ? ' ' + toReadable(num % 10) : '');
+  } else {
+    return (
+      toReadable(Math.floor(num / 100)) +
+      ' hundred' +
+      (num % 100 ? ' ' + toReadable(num % 100) : '')
+    );
+  }
+};
